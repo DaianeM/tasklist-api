6112167b3477e486551352f1fcce7d4a2fe21de7
DaianeM <daianersm2@gmail.com>
2021-07-26 08:56:17 -0300

    di

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 
+import authMiddleware from './app/middlewares/auth';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
@@ -8,5 +10,7 @@ const routes = new Router();
 routes.post('/users', UserController.store);
 
 routes.post('/sessions', SessionController.store);
+
+routes.put('/users', authMiddleware, UserController.update);
 
 export default routes;
